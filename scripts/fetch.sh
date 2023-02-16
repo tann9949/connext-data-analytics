35bc513967bf398ce4982ad1e69cc883d9eea16e
tann9949 <tan_epacsp@hotmail.com>
2023-02-16 18:34:33 +0700

    add price fetcher 🥓

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 cd ..
+
 # Fetch transactions from the network and store them in the database.
 until python fetch_txn.py;
 do
     echo "Restarting fetch_tx.py in 0.5 seconds";
+    sleep 0.5;
+done
+
+# Fetch prices from the network and store them in the database.
+until python fetch_price.py;
+do
+    echo "Restarting fetch_price.py in 0.5 seconds";
     sleep 0.5;
 done
